473f8c925f8df6ad464e4e23f4b559a4f9e834a9
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-28 11:56:20 -0600

    modificacion en la vista

--- views/permisos/index.php
+++ views/permisos/index.php
@@ -25,7 +25,7 @@
                 </select>
             </div>
         </div>
-        
+
         <div class="row mb-3">
             <div class="col">
                 <button type="submit" form="formularioPermiso" id="btnGuardar" class="btn btn-primary w-100">Guardar</button>
@@ -43,16 +43,21 @@
 
     </form>
 </div>
+
 <div id="contenedorContraseña">
     <h2>Cambiar Contraseña</h2>
     <form id="formularioContraseña">
         <div class="row mb-3">
-            <label for="usu_nombre">Ingrese un usuario</label>
-            <input type="text" name="usu_nombre" id="usu_nombre" class="form-control" required>
-
-            <label for="usu_catalogo">Catálogo</label>
-            <input type="number" name="usu_catalogo" id="usu_catalogo" class="form-control" required>
-
+            <div class="col">
+                <label for="usu_nombre">Ingrese un usuario</label>
+                <input type="text" name="usu_nombre" id="usu_nombre" class="form-control" required>
+            </div>
+            <div>
+                <label for="usu_catalogo">Catálogo</label>
+                <input type="number" name="usu_catalogo" id="usu_catalogo" class="form-control" required>
+            </div>
+        </div>
+        <div class="row mb-3">
             <div class="col">
                 <label for="usu_password">Nueva Contraseña:</label>
                 <input type="password" id="usu_password" name="usu_password" class="form-control" required>
@@ -62,9 +67,10 @@
                 <input type="password" id="usu_confirm_password" name="usu_confirm_password" class="form-control" required>
             </div>
         </div>
-        <button type="button" id="btnModificarContraseña" class="btn btn-primary">Cambiar Contraseña</button>
-        <button type="button" id="btnCancelarContraseña" class="btn btn-secondary">Cancelar</button>
-    </form>
+
+<button type="button" id="btnModificarContraseña" class="btn btn-primary">Cambiar Contraseña</button>
+<button type="button" id="btnCancelarContraseña" class="btn btn-secondary">Cancelar</button>
+</form>
 </div>
 
 
